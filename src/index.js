0a5fa8c71ae0fe918b3f6c9a6a4746e3b9cd92ab
AnMaster15 <119131473+AnMaster15@users.noreply.github.com>
2024-07-26 11:31:56 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -9,8 +9,19 @@ dotenv.config(
 )
 
 connectDB()
-
-
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("error", err);
+            throw err
+        })
+        
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("mongo connection lost", err)
+    })
 
 
 
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+class apiResponse {
+    constructor(statusCode, message, data = "Success") {
+        this.success = statusCode < 400;
+        this.message = message;
+        this.data = data;
+        this.statusCode = statusCode
+    }
+}
