beb0de649a39fc3538e5f3f03fe14fe3c7e878e4
AnMaster15 <119131473+AnMaster15@users.noreply.github.com>
2024-08-05 22:10:12 +0530

    pipeline

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
  
 import { Router } from "express"; // Corrected from 'router' to 'Router'
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js"; // Assuming the file extension is needed
+import { loginUser, logoutUser, 
+    registerUser,refreshAccessToken,
+    changeCurrentPassword,getWatchHistory,
+    getCurrentUser,updateAccountDetails,
+    updateUserAvatar,updateUserCoverImage,
+    getUserChannelProfile } from "../controllers/user.controller.js"; // Assuming the file extension is needed
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router(); // Corrected from 'router()' to 'Router()'
@@ -19,6 +24,15 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default router;
 
